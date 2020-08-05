f720fd15c1ad276b7c3f142abd08b0e03e01c7c6
ArwaAli-G <arwaaalgabas@gmail.com>
2020-08-05 19:07:53 +0300

    my comment

--- test_main.py
+++ test_main.py
@@ -14,6 +14,9 @@ PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
+    '''
+    client
+    '''
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
